6932d0c279fe1f569e6e965ea05c0481b1f33e02
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-08-31 23:04:10 +0100

    Basic ship movement

--- main.lua
+++ main.lua
@@ -1,13 +1,38 @@
 function love.load()
-
+    shipX, shipY = love.graphics.getWidth() / 2, love.graphics.getHeight() / 2
+    shipRot = 0
+    shipSpeedX, shipSpeedY = 0, 0
+    shipSpeed = 100
+    shipRotSpeed = 10
 end
 
 function love.update(dt)
+    if love.keyboard.isDown('right') then
+        shipRot = (shipRot + shipRotSpeed * dt) % (2 * math.pi)
+    end
+    if love.keyboard.isDown('left') then
+        shipRot = (shipRot - shipRotSpeed * dt) % (2 * math.pi)
+    end
+    if love.keyboard.isDown('up') then
+        shipSpeedX = shipSpeedX + math.cos(shipRot) * shipSpeed * dt
+        shipSpeedY = shipSpeedY + math.sin(shipRot) * shipSpeed * dt
+    end
 
+    shipX = shipX + shipSpeedX * dt
+    shipY = shipY + shipSpeedY * dt
 end
 
 function love.draw()
-
+    love.graphics.setColor(0, 0, 1)
+    love.graphics.circle('fill', shipX, shipY, 30)
+    love.graphics.setColor(0, 1, 1)
+    local shipCircleDistance = 20
+    love.graphics.circle(
+        'fill',
+        shipX + math.cos(shipRot) * shipCircleDistance,
+        shipY + math.sin(shipRot) * shipCircleDistance,
+        5
+    )
 end
 
 function love.keypressed(key)
